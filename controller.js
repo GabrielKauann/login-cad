7e6a3571cb70bb5bbc6dc9de1c9af7df7e4f1760
gabriel <gabrielkauan236@gmail.com>
2024-08-08 08:22:16 -0300

    Criação do botão de excluir tabela

--- controller.js
+++ controller.js
@@ -32,7 +32,14 @@ function salvarUser(){
 function criaLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadosLista.length - 1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick=''>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
+}
+
+//FUNÇÃO PARA EXCLUIR NOME DE LISTA
+
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deletRow(i);
 }
