b6c4591a4243c51daf2ce2036bff86a8bebe1a0c
gabriel <gabrielkauan236@gmail.com>
2024-08-30 09:36:30 -0300

    Correção na tabela de cadastro

--- controller.js
+++ controller.js
@@ -53,7 +53,7 @@ function acessar() {
    
     // Adiciona uma linha para cada usuário na lista
     for (let i = 0; i < dadosLista.length; i++) {
-        table += "<tr><td>" + dadosLista[i] + "</td><td>" + salvaEmail[i] + "</td><td>" + salvacpf[i] + "</td></td><td><button type='button' onclick='editar()' class='btn btn-success'>Editar</button><button class='btn btn-success' id='btnaltera' type='button' onclick='excluir()'>Excluir</button></td></tr>";
+        table += "<tr><td>" + dadosLista[i] + "</td><td>" + salvaEmail[i] + "</td><td>" + salvacpf[i] + "</td></td><td><button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)' class='btn btn-success'>Editar</button><button class='btn btn-success' id='btnaltera' type='button' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
     }
    
     // Atualiza o conteúdo da tabela com a nova lista
@@ -61,12 +61,11 @@ function acessar() {
   }
    
   // Função para editar um usuário na lista
-    function editar(){
+function editar(i) {
     // Preenche os campos de entrada com os dados do usuário selecionado
-      document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-      document.getElementById('emailUser').value = salvaEmail[(i - 1)];
-      document.getElementById('cpfUser').value = salvaCpf[(i - 1)];
-      dadosLista.splice(dadosLista[(i - 1)], 1);
+    document.getElementById('nomeUser').value = dadosLista[(i-1)];
+    document.getElementById('emailUser').value = salvaEmail[(i-1)];
+    document.getElementById('cpfUser').value = salvacpf[(i-1)];
   }
    
     // Remove o usuário da lista para permitir re-adicionar com alterações
@@ -87,3 +86,75 @@ function acessar() {
     // Remove a linha correspondente da tabela
     document.getElementById('table').deleteRow(i + 1); // +1 para ajustar o índice da tabela
   }      
+  // CODIGO DE VALIDAÇÃO DE EMAIL
+//-------------------------------------------------------------------------------------------
+function checarEmail(){
+    if(document.forms[0].email.value == "" ||
+     document.forms[0].email.value.indexOf('@') == -1||
+      document.forms[0].email.value.indexOf('.') == -1 ){
+        alert("porfavor, informe um e-mail valido");}else{
+           // alert("EMAIL INFORMADO COM SUCESSO")
+        }
+     
+  }
+  // VALIDAÇÃO DE CPF DIRETO NO JAVASCRIPT
+   
+  // Adicionando escutador ao formulário
+  document.getElementById('cpfForm').addEventListener('submit', function(event){
+    event.preventDefault();
+   
+    const cpf = document.getElementById('cpf').value;
+    const msg = document.getElementById('message');
+   
+    if(validarCPF(cpf)){
+        msg.textContent = 'O CPF é válido!';
+        msg.style.color = 'green';
+    }else{
+        msg.textContent = 'O CPF é inválido!';
+        msg.style.color = 'red';
+    }
+  }
+  );
+   
+  function validarCPF(cpf){
+    cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+   
+    // Estrutura de decisão para verificar quantidade de dígitos e se todos os digitos são iguais
+    if(cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)){
+        return false;
+    }
+   
+    let soma = 0;
+    let resto;
+   
+    // Validando 10º digito do CPF - o primeiro digito verificador
+    for(let i=1;i <= 9;i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+   
+    resto = (soma * 10) % 11;
+   
+    if((resto === 10) || (resto === 11)){
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+    // Validando 11º digito do CPF - o segundo digito verificador
+    soma = 0;
+    for(let i = 1; i <= 10; i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+    }
+   
+    resto = (soma * 10) % 11;
+   
+    if((resto === 10) || (resto === 11)){
+        resto = 0;
+    }
+   
+    if(resto !== parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+   
+    return true;
+  }
